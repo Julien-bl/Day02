1de429f4c1b5cc4e366493c3009a8f92dfdd9283
Julien-bl <julien.bulin@epitech.digital>
2024-09-03 11:51:20 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,15 @@
+let var1=typeof x
+let var2= typeof true
+let var3= typeof 2
+let var4= typeof 1234n
+let var5 = typeof Symbol()
+let var6 = typeof null
+let var7 = typeof function (){}
+
+displayThisText(var1)
+displayThisText(var2)
+displayThisText(var3)
+displayThisText(var4)
+displayThisText(var5)
+displayThisText(var6)
+displayThisText(var7)
