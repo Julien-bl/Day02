6c05dcbd6d970f8bfe57491666d8caae495be278
Julien-bl <julien.bulin@epitech.digital>
2024-09-03 15:03:52 +0200

    Create ex_08.js

---
+++ ex_08.js
@@ -0,0 +1,4 @@
+if (breadCount >= 2 && hamCount >=1 && tunaCount>=1){ displayThisText("I can make a royal sandwish!")}
+else if (breadCount >= 2 && hamCount >=1){ displayThisText("I can make a ham sandwish!")}
+else if (breadCount >= 2 && tunaCount >=1){ displayThisText("I can make a tuna sandwish!")}
+else { displayThisText("I'd rather be fasting tonight..")}
